de333b32b3c23d57dd2f82b7209e3beaa79a10ce
rodion <rodi-98@mail.ru>
2023-12-09 20:00:31 +0300

    Доп задача

---
+++ AlgoTest/lesson1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Node
+    {
+        public int value;
+        public Node next;
+        public Node(int _value) { value = _value; }
+    }
+
+    public class LinkedList
+    {
+        public Node head;
+        public Node tail;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void AddInTail(Node _item)
+        {
+            if (head == null) head = _item;
+            else tail.next = _item;
+            tail = _item;
+        }
+
+        public LinkedList SumLists(LinkedList list1, LinkedList list2)
+        {
+            //Creating linkedlist for sum value
+            LinkedList sumList = new LinkedList();
+
+            //Setting the initial list node
+            Node node1 = list1.head;
+            Node node2 = list2.head;
+
+            while (node1 != null && node2 != null)
+            {
+                //sum value
+                int sum = node1.value + node2.value;
+                sumList.AddInTail(new Node(sum));
+
+                //Setting the link
+                node1 = node1.next;
+                node2 = node2.next;
+            }
+
+            return sumList;
+        }
+    }
+}
---
+++ test/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsDataStructures;
+
+namespace LinkedListTests
+{
+    [TestClass]
+    public class LinkedListTests
+    {
+        [TestMethod]
+        public void SumLists_GetTheSumOfValuesOnLinkedLists()
+        {
+            //Создаем 1 связанный лист и добавляем значения
+            LinkedList list1 = new LinkedList();
+            list1.AddInTail(new Node(1));
+            list1.AddInTail(new Node(2));
+            list1.AddInTail(new Node(3));
+            //Создаем 2 связанный лист и добавляем значения
+            LinkedList list2 = new LinkedList();
+            list2.AddInTail(new Node(4));
+            list2.AddInTail(new Node(5));
+            list2.AddInTail(new Node(6));
+
+            LinkedList sumList = list1.SumLists(list1, list2);
+
+            //Проверяем суммы по узлам листа
+            Assert.AreEqual(5, sumList.head.value);
+            Assert.AreEqual(7, sumList.head.next.value);
+            Assert.AreEqual(9, sumList.tail.value);
+            //Конечный узел
+            Assert.AreEqual(null, sumList.tail.next);
+        }
+    }
+}
