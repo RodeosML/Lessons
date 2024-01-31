9c749388d4f3591742f318e74930735378d38381
rodion <rodi-98@mail.ru>
2024-01-31 22:08:00 +0300

    Первое занятие

---
+++ AlgoTest/Clear code/Lesson 1.cs
@@ -0,0 +1,24 @@
+﻿totalRevenueInDollar - totalRevenueInUSD // общий доход в долларах
+
+gapGasCub - demandSupplyGapInGasEqivavlent // разрыв между спросом и предложением в газовом эквиваленте 
+
+yearsProfitMargin % - percentageOfProfitMarginByYear // массив процентной маржи прибыли  
+
+inflation - inflationIndexOfCostGas // индекс уровня инфляции в балансе стоимости газа - значение
+
+price$ - priceInUSD // цена в долларах - значение
+
+bondYear - currentYearBondYieldInPercentage // Массив доходностей облигаций текущего года
+
+sumSalary - amountOfEmployeeSalaries // Массив сумм заработных плат сотрудников
+
+indexCo - indexCompany // Индекс компании в массиве компаний
+
+inputNDPI - inputCompanyNDPI // Задает переменную, которая хранит значение НДПИ (налог на добычу полезных ископаемых) по компании
+
+totalTime - inputTotalHours // Задает переменную, которая хранит значение часов
+
+rotateValue - inputDebitorTurnoverRate //Задает переменную, которая хранит значение дебиторской оборачиваемости
+
+tempF - temperatureInFahrenheit //константа температуры в Фаренгейтах - значение
+
