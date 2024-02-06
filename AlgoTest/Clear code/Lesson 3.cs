ab176a1f0e4bf2b5270356b8097a2a0a189b41d7
rodion <rodi-98@mail.ru>
2024-02-06 22:11:00 +0300

    Lesson 3

---
+++ AlgoTest/Clear code/Lesson 3.cs
@@ -0,0 +1,33 @@
+﻿//7.1
+dataLoaded - isDataLoaded;
+permissionGranted - isPermission;
+networkConnected - isConnected;
+emailValid - isValidEmail;
+completeData - isCompleteData;
+
+//7.2
+isInitialized - initialized
+isUploaded - uploaded
+isValid - valid
+isCompleted - completed
+isInstalled - installed
+
+//7.3
+В цикле прохождения по годам использовалась переменная i и использовалась несколько раз внутри цикла.
+Для наглядности можно было бы использовать имя year.
+
+//7.4
+isPaused/isResumed
+isMaximized/isMinimized
+isOpened/isClosed
+isCancelled/isConfirmed
+isSubscribed/isUnsubscribed
+
+//7.5
+length - numberOfElements
+temp - temporaryVariable
+data - retrievedUserData
+value - currentSelectedValue
+result - calculationResult
+
+
