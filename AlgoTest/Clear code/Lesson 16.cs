be47874fc3803148b02934ff6ff4f316392d0bbf
rodion <rodi-98@mail.ru>
2024-03-16 09:55:35 +0300

    Lesson 16

---
+++ AlgoTest/Clear code/Lesson 16.cs
@@ -0,0 +1,114 @@
+﻿//1 Шум. Не несут смысловой информации, можно удалить:
+//Метод для разложения листа в иерерахию
+var hierarchy = Methods.CreateHierarchy(targetList);
+
+//2 Шум. Не несет смысловой информации, можно удалить:
+//Переводим поток под ClosedXML
+var workbook = new XLWorkbook(stream);
+
+//3 Шум и из контекста и так все понятно. Не несет смысловой информации, можно удалить:
+//Обновляем информацию об организациях:
+organization.refreshInfo()
+
+//4 Избыточная информация. По названию функции и так понятно, какую цель преследует функция.
+//Получаем сведения о бухгалтерских отчетах компании
+getfinancialstatementsForOrganization()
+
+
+//5 Позиционные маркеры. В данном случае лучше заменить на TO DO
+
+///////////////////////REMAKE/////////////////
+  private getYesterdayIf(day: string): Date {
+    return day === 'yesterday' ? new Date(Date.now() - 86400000) : new Date();
+  }
+
+//6 Шум, а также изыбыточное описание для запроса.
+
+//Получение пользовательских ролей
+public class GetUserRoles {}
+
+//7 Шум, а также изыбыточное описание для запроса.
+
+//Получение всех миссий для главной страницы
+private getAllMissions(
+searchTerm: string = '',
+startTime: any = '',
+    endTime: any = ''
+  ) {
+    this.mainService
+      .getAllMissions()
+      .pipe(
+        ...more code)
+
+//8 Недостоверный комментарий. Данный код не имеет отношение к фильтрации данных
+
+    // FILTER
+        if (startDate && endDate)
+    {
+        // FORMAT MS
+        const startDateObj = DateUtils.formatDateToUnixTimestamp(startDate);
+        const endDateObj = DateUtils.formatDateToUnixTimestamp(endDate);
+    }
+
+//9 Лучше всего будет преобразовать комментарий для понимания контекста в название функции.
+
+//Было:
+// Преобразование [] миссий в поток значений
+private transformMissions(missions: Mission[]) {
+
+//Стало:
+private convertArrayMissionsIntoStreamValues(missions: Mission[]) {
+
+//10 Шум
+// UPD SEL MISSION
+this.timerManagementService.updateSelectedMissionz(mission);
+
+//11 Шум
+// Создаем [] объектов
+toArray(),
+    map((group) => ({ group, missions }))
+
+//12 Шум
+// SEARCH
+if (searchTerm)
+            {
+                missions = missions.filter((mission) => {
+                    return mission.title
+                      .toLowerCase()
+                      .includes(searchTerm.toLowerCase());
+                });
+
+//13 Шум. Данный код лежит в папке, которая относиться к конкретной фиче.
+// SEARCH AND AUTOCOMPLETE$
+ private handleSearchTermChanges(searchTerm: string) {
+ this.filterDataService.updateFilteredQuery(searchTerm);
+ this.isDirty = !!searchTerm;
+
+//14 Был найден закомментированный старый код, который уже не измеет значение и его можно удалить.
+
+// confirmMission() {
+//   const selectedMissionz = this.timerManagementService.getSelectedMissionz();
+
+//   for (const mission of selectedMissionz) {
+//     this.mainService
+//       .confirmMission(mission.id)
+//       .pipe(
+//         tap(() => {
+//           this.popupService.closePopup();
+//         })
+//       )
+//       .subscribe();
+//   }
+// }
+//15 Шум. Из контекста и так понятно, что выполняет данная функция в коде
+//CHECK NAN
+if (isNaN(diffHours) || isNaN(diffMinutesPart))
+ {
+  diffHours = '...';
+                        diffMinutesPart = '...';
+}
+else
+{
+  diffHours = String(diffHours);
+  diffMinutesPart = String(diffMinutesPart);
+}
